1806c3dd36dfcfd3020fa659c5cc91e249d774fb
rohnoan <sharmarohan2507@gmail.com>
2024-09-06 01:05:16 +0530

    added hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Hero from './components/Hero'
+import Generator from './components/Generator'
+import Workout from './components/Workout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-white bg-black'>
-      hi
-    </div>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+      <Hero/>
+      <Generator/>
+      <Workout/>
+    </main>
   )
 }
 
