74f7e0cf29d075e4add8a1bb34cd9482a30ba9bb
rohnoan <sharmarohan2507@gmail.com>
2024-09-06 01:31:46 +0530

    added sectionwrapper and backend file

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
 
 export default function Generator() {
   return (
-    <div>
-      generator
-    </div>
+    <SectionWrapper header={"generate your workout"} 
+    title={['it`s','huge','o`clock']}>
+      dasfg
+    </SectionWrapper>
   )
 }
