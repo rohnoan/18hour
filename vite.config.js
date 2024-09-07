893c8a07f4412144e933a63e73ab83fa7684bd2a
rohnoan <sharmarohan2507@gmail.com>
2024-09-07 11:33:07 +0530

    changed vite confid

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/18hour/', // Add this line
+  base: '/gym-app/', // Add this line
 })
